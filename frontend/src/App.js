d42b0e197e9a3985310ffdf472993a660b96500f
Muskan Koshti <103520140+muskank101@users.noreply.github.com>
2024-03-22 20:32:18 +0530

    home
    completed the home page i.e., navbar and localhost:3000

---
+++ frontend/src/App.js
@@ -0,0 +1,102 @@
+import { createContext, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Grid } from '@mui/material';
+import {makeStyles} from "@mui/styles";
+import Welcome from "./component/Welcome";
+import Navbar from "./Navbar"
+const useStyles = makeStyles((theme) => ({
+  body: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "98vh",
+    paddingTop: "64px",
+    boxSizing: "border-box",
+    width: "100%",
+  },
+}));
+
+export const SetPopupContext = createContext();
+
+function App() {
+  const classes = useStyles();
+  const [popup, setPopup] = useState({
+    open: false,
+    severity: "",
+    message: "",
+  });
+  return (
+    <BrowserRouter>
+      <SetPopupContext.Provider value={setPopup}>
+        <Grid conatiner direction="column">
+          <Grid item xs>
+            <Navbar/>
+          </Grid>
+          <Grid item className={classes.body}>
+
+
+            <Routes>
+              {/* <Route exact path="/">
+              element=
+                <{<Welcome/>} /> */}
+              <Route path="/" element={<Welcome />} />
+              {/* </Route> */}
+              <Route exact path="/login">
+                {/* <Login /> */}
+              </Route>
+              <Route exact path="/signup">
+                {/* <Signup /> */}
+              </Route>
+              <Route exact path="/logout">
+                {/* <Logout /> */}
+              </Route>
+              <Route exact path="/home">
+                {/* <Home /> */}
+              </Route>
+              <Route exact path="/applications">
+                {/* <Applications /> */}
+              </Route>
+              {/* <Route exact path="/profile">
+                {userType() === "recruiter" ? (
+                  <RecruiterProfile />
+                ) : (
+                  <Profile />
+                )}
+              </Route> */}
+              <Route exact path="/addjob">
+                {/* <CreateJobs />
+              {/* </Route>
+              <Route exact path="/myjobs">
+                {/* <MyJobs /> */}
+              </Route>
+              <Route exact path="/job/applications/:jobId">
+                {/* <JobApplications /> */}
+              </Route>
+              <Route exact path="/employees">
+                {/* <AcceptedApplicants /> */}
+              </Route>
+              <Route>
+                {/* <ErrorPage /> */}
+              </Route> */} */}
+            </Routes>
+          </Grid>
+        </Grid>
+        {/* <MessagePopup
+          open={popup.open}
+          setOpen={(status) =>
+            setPopup({
+              ...popup,
+              open: status,
+            })
+          }
+          severity={popup.severity}
+          message={popup.message}
+        /> */}
+      </SetPopupContext.Provider>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
---
+++ frontend/src/component/Welcome.js
@@ -0,0 +1,39 @@
+// import { Grid, Typography } from "@mui/material";
+// import { Grid, Typography } from"@mui/core";
+import { Grid } from '@mui/material';
+import { Typography } from '@mui/material';
+const Welcome = (props) => {
+  return (
+    <Grid
+      container
+      item
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ padding: "30px", minHeight: "10vh" }}
+    >
+      <Grid item>
+        <Typography variant="h2">Welcome to Job Portal</Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+export const ErrorPage = (props) => {
+  return (
+    <Grid
+      container
+      item
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ padding: "30px", minHeight: "93vh" }}
+    >
+      <Grid item>
+        <Typography variant="h2">Error 404</Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Welcome;
